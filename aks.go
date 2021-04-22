4974a215c6101120552bfbd8f1ae61543e9aed62
Ian Tivey <ian.tivey@citihub.com>
2021-04-22 12:56:37 +0100

    Move Azure Managed Identity features to AKS Service Pack

--- aks.go
+++ aks.go
@@ -2,6 +2,7 @@ package pack
 
 import (
 	azureaks "github.com/citihub/probr-pack-aks/internal/azure/aks"
+	kubeaks "github.com/citihub/probr-pack-aks/internal/azure/kubernetes/iam"
 	//"github.com/citihub/probr-sdk/config"
 	"github.com/citihub/probr-sdk/probeengine"
 	"github.com/markbates/pkger"
@@ -16,6 +17,7 @@ func GetProbes() []probeengine.Probe {
 	}*/
 	return []probeengine.Probe{
 		azureaks.Probe,
+		kubeaks.Probe,
 	}
 }
 
@@ -24,4 +26,5 @@ func init() {
 	// See: https://github.com/markbates/pkger
 	pkger.Include("/internal/azure/aks/aks.feature")
 	pkger.Include("/internal/azure/aks/aks.rego")
+	pkger.Include("/internal/azure/kubernetes/iam/iam.feature")
 }
